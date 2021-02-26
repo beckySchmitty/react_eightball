335e3ccee1146423d159be6ac8dff25526d6e7f8
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-02-26 12:32:29 -0500

    EightBall msg/color change working

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 
 const EightBall = ({answers}) => {
-    const msg = "Think of a Question"
+    const [msg, changeMsg] = useState('Think of a Question');
+    const [color, changeColor] = useState('black')
+
+    const ranAnswer = () => answers[Math.floor(Math.random() * answers.length)];
+
+    const clickedBall = () => {
+        changeMsg(ranAnswer().msg);
+        changeColor(ranAnswer().color);
+    }
+
+    const btnStyle = {
+        backgroundColor: `${color}`,
+        color: 'white'
+    }
     
     return (
         <div className="EightBall">
-            <h1>WORKING!!!</h1>
-            <button className="EightBall-btn">{msg}</button>
-            <p>{answers[0].msg}</p>
+            <h1>Magic Eight Ball</h1>
+            <button onClick={clickedBall} style={btnStyle} className="EightBall-btn">{msg}</button>
         </div>
     )
 }
